18cfd276c35f157f2683d30adb7b8cdf8f6a1f9e
Nikita Zemtsov <z-nik@ya.ru>
2021-01-31 18:48:28 +0100

    create basic scene and import player model (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use dotrix::{
     Dotrix,
-    assets::Texture,
-    components::SkyBox,
+    assets:: {Texture, Mesh},
+    components:: {SkyBox, Model, Light},
     ecs::{ Mut, RunLevel, System },
     input::{ ActionMapper, Button, Mapper },
     services::{ Assets, Camera, Frame, Input, World },
     systems::{ camera_control, world_renderer },
+    renderer::transform::Transform,
+    math::{ Vec3, Point3, },
 };
 
 fn main() {
@@ -19,8 +21,10 @@ fn main() {
         .with_service(Frame::new())
         .with_service(
             Camera {
-                distance: 1.0,
-                xz_angle: 0.0,
+                distance: 10.0,
+                y_angle: 0.5,
+                xz_angle: 0.0,                
+                target: Point3::new(0.0, 2.0, 0.0),
                 ..Default::default()
             }
         )
@@ -31,6 +35,18 @@ fn main() {
 }
 
 fn startup(mut world: Mut<World>, mut assets: Mut<Assets>) {
+    
+    init_skybox(&mut world, &mut assets);
+    init_terrain(&mut world, &mut assets);
+    init_player(&mut world, &mut assets);
+    init_light(&mut world);    
+
+}
+
+fn init_skybox(
+    world: &mut World,
+    assets: &mut Assets,
+) {
     let primary_texture = [
         assets.register::<Texture>("skybox_right"),
         assets.register::<Texture>("skybox_left"),
@@ -52,6 +68,101 @@ fn startup(mut world: Mut<World>, mut assets: Mut<Assets>) {
     world.spawn(vec![
         (SkyBox { primary_texture, ..Default::default() },),
     ]);
+}
+
+fn init_terrain(
+    world: &mut World,
+    assets: &mut Assets,
+) {
+    // Generate terrain mesh like this:
+    //   0   1
+    // 0 +---+---+---> x
+    //   | / | / |
+    // 1 +---+---+
+    //   | / | / |
+    //   +---+---+
+    //   |
+    //   z
+
+    let size = 64;
+    let mut positions = Vec::with_capacity(3 * 2 * size * size);
+    let mut uvs = Vec::new();
+    for x in 0..size {
+        let x0 = x as f32;
+        let x1 = x0 + 1.0;
+        for z in 0..size {
+            let z0 = z as f32;
+            let z1 = z0 + 1.0;
+            // Add vertices
+            positions.push([x0, 0.0, z0]);
+            positions.push([x0, 0.0, z1]);
+            positions.push([x1, 0.0, z0]);
+            positions.push([x1, 0.0, z0]);
+            positions.push([x0, 0.0, z1]);
+            positions.push([x1, 0.0, z1]);
+            // Add texture vertices
+            uvs.push([0.0, 0.0]);
+            uvs.push([0.0, 1.0]);
+            uvs.push([1.0, 0.0]);
+            uvs.push([1.0, 0.0]);
+            uvs.push([0.0, 1.0]);
+            uvs.push([1.0, 1.0]);
+        }
+    }
+
+    let mut mesh = Mesh {
+        positions,
+        uvs: Some(uvs),
+        ..Default::default()
+    };
+    // Calculate mesh normals
+    mesh.calculate();
+
+    // Store mesh and get its ID
+    let mesh = assets.store(mesh, "terrain");
+
+    // import terrain texture and get its ID
+    assets.import("assets/terrain/terrain.png");
+    let texture = assets.register("terrain");
+
+    // Center terrain tile at coordinate system center (0.0, 0.0, 0.0) by moving the tile on a
+    // half of its size by X and Z axis
+    let shift = (size / 2) as f32;
+    let transform = Transform {
+        translate: Vec3::new(-shift, 0.0, -shift),
+        ..Default::default()
+    };
+
+    // Spawn terrain in the world
+    world.spawn(Some(
+        (Model { mesh, texture, transform, ..Default::default() },)
+    ));
+}
+
+fn init_player(
+    world: &mut World,
+    assets: &mut Assets,
+) {
+    let texture = assets.register::<Texture>("player::texture");
+    let mesh = assets.register::<Mesh>("player::mesh");   
+
+    assets.import("assets/player/player.gltf");
+
+    let trans = Transform {
+        translate: Vec3::new(0.0, 1.0, 0.0),
+        ..Default::default()
+    };
+
+    // spawn model in the world
+    world.spawn(Some(
+        (
+            Model { mesh, texture, transform: trans, ..Default::default() },
+        ),
+    ));
+}
+
+fn init_light(world: &mut World) {
+    world.spawn(Some((Light::white([25.0, 100.0, 25.0]),)));
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
