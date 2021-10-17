998c66ceaa535447bde753ab4f3cc0589918af36
Nikita Zemtsov <z-nik@ya.ru>
2021-10-17 12:56:58 +0200

    Move window to center and resize to 1280x720 (#10)

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,19 @@
 use dotrix::ecs::{ Mut };
 use dotrix::{ Window };
+use dotrix::math::{ Vec2i, Vec2u };
 
 pub fn startup(
     mut window: Mut<Window>,
 ) {
     window.set_cursor_grab(true);
     window.set_cursor_visible(false);
+
+    window.set_outer_position(
+        Vec2i::new(
+            (window.screen_size().x - window.outer_size().x) as i32 / 2,
+            (window.screen_size().y - window.outer_size().y) as i32 / 2,
+        )
+    );
+
+    window.set_inner_size(Vec2u::new(1280, 720));
 }
